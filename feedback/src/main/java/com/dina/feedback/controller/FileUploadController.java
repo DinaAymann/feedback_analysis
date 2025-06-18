658bb1cd81a313f0397f4defc766e74a132fccac
DinaAymann <dinaayman804@gmail.com>
2025-06-18 12:11:57 +0300

    upload

--- feedback/src/main/java/com/dina/feedback/controller/FileUploadController.java
+++ feedback/src/main/java/com/dina/feedback/controller/FileUploadController.java
@@ -1,38 +1,4 @@
-//package com.dina.feedback.controller;
-//
-//import com.dina.feedback.service.FileStorageService;
-//import com.dina.feedback.service.FeedbackJobLauncher;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;a
-//import org.springframework.web.multipart.MultipartFile;
-//
-//@RestController
-//@RequestMapping("/api/files")
-//public class FileUploadController {
-//
-//    @Autowired
-//    private FileStorageService fileStorageService;
-//    @Autowired
-//    private FeedbackJobLauncher feedbackJobLauncher;
-//
-//    @PostMapping("/upload")
-//    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
-//        String filename = file.getOriginalFilename();
-//        if (fileStorageService.fileExists(filename)) {
-//            return ResponseEntity.badRequest().body("❌ File already exists.");
-//        }
-//        try {
-//            fileStorageService.saveFile(file);
-//            feedbackJobLauncher.launchJob(filename);
-//            return ResponseEntity.ok("✅ File uploaded successfully.");
-//        }  catch (Exception e) {
-//            e.printStackTrace();  // ← Add this to see full stack trace in console
-//            return ResponseEntity.internalServerError().body("❌ Upload failed: " + e.getMessage());
-//    }
-//
-//}
-//}
+
 
 package com.dina.feedback.controller;
 
